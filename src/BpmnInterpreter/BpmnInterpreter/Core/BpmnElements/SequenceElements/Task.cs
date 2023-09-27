ee027988f6d0bf435d75c291cba38deadaf6114f
Samet Yazıcı <sametyzc@hotmail.com>
2023-09-27 17:49:38 +0300

    feat(sequence): Add 'Task' class for BPMN Task Element
    The 'Task' class represents a BPMN task element

---
+++ src/BpmnInterpreter/BpmnInterpreter/Core/BpmnElements/SequenceElements/Task.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Linq;
+
+namespace BpmnInterpreter.Core.BpmnElements.SequenceElements
+{
+    public class Task : BpmnSequenceElement
+    {
+        public static readonly string ElementTypeName = "task";
+
+        public Task(
+            XElement self,
+            IEnumerable<BpmnSequenceElement>? previousElements = null,
+            IEnumerable<BpmnSequenceElement>? nextElements = null
+        ) : base(self, previousElements, nextElements)
+        {
+
+        }
+    }
+}
