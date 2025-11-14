4830d177c30f81569bbdb9ce6b6d687a0c1bd4ae
YKACARNTB\ykacar <ykacar@trabilisim.com>
2025-11-14 13:36:37 +0300

    subprocess element type added

--- src/BpmnInterpreter/Playground/Program.cs
+++ src/BpmnInterpreter/Playground/Program.cs
@@ -23,150 +23,72 @@ namespace Playground
         static void Main(string[] args)
         {
             var xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>
-<bpmn:definitions xmlns:bpmn=""http://www.omg.org/spec/BPMN/20100524/MODEL"" xmlns:tra=""http://tra"" xmlns:bpmndi=""http://www.omg.org/spec/BPMN/20100524/DI"" xmlns:dc=""http://www.omg.org/spec/DD/20100524/DC"" xmlns:di=""http://www.omg.org/spec/DD/20100524/DI"">
-  <bpmn:process id=""Process_main"">
-    <bpmn:startEvent id=""Event_1a20tjs"">
-      <bpmn:extensionElements>
-        <tra:crons>
-          <tra:cron expression=""0 0/30 * ? * * *"" />
-        </tra:crons>
-      </bpmn:extensionElements>
-      <bpmn:outgoing>Flow_17n3a2y</bpmn:outgoing>
-      <bpmn:timerEventDefinition id=""TimerEventDefinition_0mxmwc9"" />
+<bpmn:definitions xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:bpmn=""http://www.omg.org/spec/BPMN/20100524/MODEL"" xmlns:bpmndi=""http://www.omg.org/spec/BPMN/20100524/DI"" xmlns:dc=""http://www.omg.org/spec/DD/20100524/DC"" xmlns:di=""http://www.omg.org/spec/DD/20100524/DI"" id=""Definitions_1sb21a0"" targetNamespace=""http://bpmn.io/schema/bpmn"" exporter=""bpmn-js (https://demo.bpmn.io)"" exporterVersion=""18.6.1"">
+  <bpmn:process id=""Process_1dj2y4l"" isExecutable=""false"">
+    <bpmn:startEvent id=""StartEvent_1acrn3m"">
+      <bpmn:outgoing>Flow_0g8wv39</bpmn:outgoing>
     </bpmn:startEvent>
-    <bpmn:task id=""kredisqlbuilder"" tra:subtype=""sqlBuilder"" name=""kredisqlbuilder"">
-      <bpmn:extensionElements>
-        <tra:sqlBuilderFrom dataSource=""1"" tableName=""dbo.Kredi"" />
-        <tra:sqlBuilderJoin sourceColumn="""" targetTable="""" targetColumn="""" />
-        <tra:sqlBuilderSelect>
-          <tra:sqlBuilderSelectColumn column=""dbo.Kredi.CustomerName"" type=""string"" alias=""DboKrediCustomerName"" />
-          <tra:sqlBuilderSelectColumn column=""dbo.Kredi.OrderNumber"" type=""number"" alias=""DboKrediOrderNumber"" />
-          <tra:sqlBuilderSelectColumn column=""dbo.Kredi.TotalCredit"" type=""number"" alias=""DboKrediTotalCredit"" />
-        </tra:sqlBuilderSelect>
-        <tra:sqlBuilderWhere />
-        <tra:sqlBuilderLimit limit="""" />
-        <tra:outputMapping saveOutput=""false"" expression=""{&#10;  &#34;data&#34;: output&#10;}"" parsedExpression=""eyJ0eXBlIjoiT2JqZWN0RXhwcmVzc2lvbiIsInByb3BlcnRpZXMiOlt7InR5cGUiOiJQcm9wZXJ0eSIsImNvbXB1dGVkIjpmYWxzZSwia2V5Ijp7InR5cGUiOiJMaXRlcmFsIiwidmFsdWUiOiJkYXRhIiwicmF3IjoiXCJkYXRhXCIifSwidmFsdWUiOnsidHlwZSI6IklkZW50aWZpZXIiLCJuYW1lIjoib3V0cHV0In0sInNob3J0aGFuZCI6ZmFsc2V9XX0="" />
-      </bpmn:extensionElements>
-      <bpmn:incoming>Flow_17n3a2y</bpmn:incoming>
-      <bpmn:outgoing>Flow_1f35s69</bpmn:outgoing>
-    </bpmn:task>
-    <bpmn:task id=""krediordernumberfilter"" tra:subtype=""filter"" name=""krediordernumberfilter"">
-      <bpmn:extensionElements>
-        <tra:inputSelector selectedInput=""kredisqlbuilder.data"" />
-        <tra:filters>
-          <tra:filter name=""order"" filterType=""builder"" expression=""DboKrediOrderNumber == (18)"" parsedExpression=""eyJ0eXBlIjoiQmluYXJ5RXhwcmVzc2lvbiIsIm9wZXJhdG9yIjoiPT0iLCJsZWZ0Ijp7InR5cGUiOiJJZGVudGlmaWVyIiwibmFtZSI6IkRib0tyZWRpT3JkZXJOdW1iZXIifSwicmlnaHQiOnsidHlwZSI6IkxpdGVyYWwiLCJ2YWx1ZSI6MTgsInJhdyI6IjE4In19"" />
-        </tra:filters>
-        <tra:outputMapping saveOutput=""false"" expression=""{&#10;  &#34;data&#34;: output&#10;}"" parsedExpression=""eyJ0eXBlIjoiT2JqZWN0RXhwcmVzc2lvbiIsInByb3BlcnRpZXMiOlt7InR5cGUiOiJQcm9wZXJ0eSIsImNvbXB1dGVkIjpmYWxzZSwia2V5Ijp7InR5cGUiOiJMaXRlcmFsIiwidmFsdWUiOiJkYXRhIiwicmF3IjoiXCJkYXRhXCIifSwidmFsdWUiOnsidHlwZSI6IklkZW50aWZpZXIiLCJuYW1lIjoib3V0cHV0In0sInNob3J0aGFuZCI6ZmFsc2V9XX0="" />
-      </bpmn:extensionElements>
-      <bpmn:incoming>Flow_1f35s69</bpmn:incoming>
-      <bpmn:outgoing>Flow_05kznqg</bpmn:outgoing>
-    </bpmn:task>
-    <bpmn:sequenceFlow id=""Flow_17n3a2y"" sourceRef=""Event_1a20tjs"" targetRef=""kredisqlbuilder"" />
-    <bpmn:sequenceFlow id=""Flow_1f35s69"" sourceRef=""kredisqlbuilder"" targetRef=""krediordernumberfilter"" />
-    <bpmn:task id=""map2items"" tra:subtype=""map"">
-      <bpmn:extensionElements>
-        <tra:inputSelector selectedInput=""MessageEventId.krediordernumberfilter.data"" />
-        <tra:maps>
-          <tra:map variableName=""DboKrediCustomerName"" formula=""DboKrediCustomerName"" formulaExpression=""eyJ0eXBlIjoiSWRlbnRpZmllciIsIm5hbWUiOiJEYm9LcmVkaUN1c3RvbWVyTmFtZSJ9"" />
-          <tra:map variableName=""DboKrediOrderNumber"" formula=""DboKrediOrderNumber"" formulaExpression=""eyJ0eXBlIjoiSWRlbnRpZmllciIsIm5hbWUiOiJEYm9LcmVkaU9yZGVyTnVtYmVyIn0="" />
-          <tra:map variableName=""DboKrediTotalCredit"" formula="""" formulaExpression=""eyJ0eXBlIjoiQ29tcG91bmQiLCJib2R5IjpbXX0="" />
-        </tra:maps>
-        <tra:outputMapping saveOutput=""false"" expression=""{&#10;  &#34;data&#34;: output&#10;}"" parsedExpression=""eyJ0eXBlIjoiT2JqZWN0RXhwcmVzc2lvbiIsInByb3BlcnRpZXMiOlt7InR5cGUiOiJQcm9wZXJ0eSIsImNvbXB1dGVkIjpmYWxzZSwia2V5Ijp7InR5cGUiOiJMaXRlcmFsIiwidmFsdWUiOiJkYXRhIiwicmF3IjoiXCJkYXRhXCIifSwidmFsdWUiOnsidHlwZSI6IklkZW50aWZpZXIiLCJuYW1lIjoib3V0cHV0In0sInNob3J0aGFuZCI6ZmFsc2V9XX0="" />
-      </bpmn:extensionElements>
-      <bpmn:incoming>Flow_13vyc9a</bpmn:incoming>
-      <bpmn:outgoing>Flow_1qj8tpk</bpmn:outgoing>
-    </bpmn:task>
-    <bpmn:intermediateCatchEvent id=""MessageEventId"">
-      <bpmn:extensionElements>
-        <tra:mail to=""ykacar@trabilisim.com; yarenozlemkacar@gmail.com; "" cc=""syazici@trabilisim.com; "" subject=""Konu"" body=""Body"" output=""krediordernumberfilter.data"" />
-      </bpmn:extensionElements>
-      <bpmn:incoming>Flow_05kznqg</bpmn:incoming>
-      <bpmn:outgoing>Flow_13vyc9a</bpmn:outgoing>
-      <bpmn:messageEventDefinition id=""MessageEventDefinition_0o53lno"" />
-    </bpmn:intermediateCatchEvent>
-    <bpmn:sequenceFlow id=""Flow_05kznqg"" sourceRef=""krediordernumberfilter"" targetRef=""MessageEventId"" />
-    <bpmn:task id=""sontask"" tra:subtype=""map"">
-      <bpmn:extensionElements>
-        <tra:inputSelector selectedInput=""map2items.data"" />
-        <tra:maps>
-          <tra:map variableName=""customer"" formula=""DboKrediCustomerName"" formulaExpression=""eyJ0eXBlIjoiSWRlbnRpZmllciIsIm5hbWUiOiJEYm9LcmVkaUN1c3RvbWVyTmFtZSJ9"" />
-        </tra:maps>
-        <tra:outputMapping saveOutput=""false"" expression=""{&#10;  &#34;data&#34;: output&#10;}"" parsedExpression=""eyJ0eXBlIjoiT2JqZWN0RXhwcmVzc2lvbiIsInByb3BlcnRpZXMiOlt7InR5cGUiOiJQcm9wZXJ0eSIsImNvbXB1dGVkIjpmYWxzZSwia2V5Ijp7InR5cGUiOiJMaXRlcmFsIiwidmFsdWUiOiJkYXRhIiwicmF3IjoiXCJkYXRhXCIifSwidmFsdWUiOnsidHlwZSI6IklkZW50aWZpZXIiLCJuYW1lIjoib3V0cHV0In0sInNob3J0aGFuZCI6ZmFsc2V9XX0="" />
-      </bpmn:extensionElements>
-      <bpmn:incoming>Flow_1qj8tpk</bpmn:incoming>
-      <bpmn:outgoing>Flow_1n6d1n8</bpmn:outgoing>
-    </bpmn:task>
-    <bpmn:sequenceFlow id=""Flow_13vyc9a"" sourceRef=""MessageEventId"" targetRef=""map2items"" />
-    <bpmn:sequenceFlow id=""Flow_1qj8tpk"" sourceRef=""map2items"" targetRef=""sontask"" />
-    <bpmn:endEvent id=""Event_0ss5h3c"">
-      <bpmn:incoming>Flow_1n6d1n8</bpmn:incoming>
+    <bpmn:endEvent id=""Event_16pofqx"">
+      <bpmn:incoming>Flow_1vdwbwx</bpmn:incoming>
     </bpmn:endEvent>
-    <bpmn:sequenceFlow id=""Flow_1n6d1n8"" sourceRef=""sontask"" targetRef=""Event_0ss5h3c"" />
-    <bpmn:boundaryEvent id=""Event_085mbwq"" attachedToRef=""map2items"">
-      <bpmn:extensionElements>
-        <tra:mail to=""ykacar@trabilisim.com; syazici@trabilisim.com; "" cc=""cturkan@trabilisim.com; "" subject=""Konu"" body=""gövde"" output=""map2items.data"" />
-      </bpmn:extensionElements>
-      <bpmn:messageEventDefinition id=""MessageEventDefinition_0aky2kp"" />
-    </bpmn:boundaryEvent>
+    <bpmn:subProcess id=""Activity_10s84on"">
+      <bpmn:incoming>Flow_0g8wv39</bpmn:incoming>
+      <bpmn:outgoing>Flow_1vdwbwx</bpmn:outgoing>
+      <bpmn:startEvent id=""Event_19p8um1"">
+        <bpmn:outgoing>Flow_11x64re</bpmn:outgoing>
+      </bpmn:startEvent>
+      <bpmn:endEvent id=""Event_1cgqww1"">
+        <bpmn:incoming>Flow_0h4nf3f</bpmn:incoming>
+      </bpmn:endEvent>
+      <bpmn:task id=""Activity_13n23id"">
+        <bpmn:incoming>Flow_11x64re</bpmn:incoming>
+        <bpmn:outgoing>Flow_0h4nf3f</bpmn:outgoing>
+      </bpmn:task>
+      <bpmn:sequenceFlow id=""Flow_11x64re"" sourceRef=""Event_19p8um1"" targetRef=""Activity_13n23id"" />
+      <bpmn:sequenceFlow id=""Flow_0h4nf3f"" sourceRef=""Activity_13n23id"" targetRef=""Event_1cgqww1"" />
+    </bpmn:subProcess>
+    <bpmn:sequenceFlow id=""Flow_0g8wv39"" sourceRef=""StartEvent_1acrn3m"" targetRef=""Activity_10s84on"" />
+    <bpmn:sequenceFlow id=""Flow_1vdwbwx"" sourceRef=""Activity_10s84on"" targetRef=""Event_16pofqx"" />
   </bpmn:process>
   <bpmndi:BPMNDiagram id=""BPMNDiagram_1"">
-    <bpmndi:BPMNPlane id=""BPMNPlane_1"" bpmnElement=""Process_main"">
-      <bpmndi:BPMNShape id=""Event_1a20tjs_di"" bpmnElement=""Event_1a20tjs"">
-        <dc:Bounds x=""272"" y=""112"" width=""36"" height=""36"" />
+    <bpmndi:BPMNPlane id=""BPMNPlane_1"" bpmnElement=""Process_1dj2y4l"">
+      <bpmndi:BPMNShape id=""_BPMNShape_StartEvent_2"" bpmnElement=""StartEvent_1acrn3m"">
+        <dc:Bounds x=""156"" y=""82"" width=""36"" height=""36"" />
       </bpmndi:BPMNShape>
-      <bpmndi:BPMNShape id=""Activity_11snd2o_di"" bpmnElement=""kredisqlbuilder"">
-        <dc:Bounds x=""240"" y=""190"" width=""100"" height=""80"" />
-        <bpmndi:BPMNLabel />
+      <bpmndi:BPMNShape id=""Event_16pofqx_di"" bpmnElement=""Event_16pofqx"">
+        <dc:Bounds x=""702"" y=""72"" width=""36"" height=""36"" />
       </bpmndi:BPMNShape>
-      <bpmndi:BPMNShape id=""Activity_140fyfl_di"" bpmnElement=""krediordernumberfilter"">
-        <dc:Bounds x=""240"" y=""300"" width=""100"" height=""80"" />
-        <bpmndi:BPMNLabel />
+      <bpmndi:BPMNShape id=""Activity_10s84on_di"" bpmnElement=""Activity_10s84on"" isExpanded=""true"">
+        <dc:Bounds x=""230"" y=""40"" width=""350"" height=""200"" />
       </bpmndi:BPMNShape>
-      <bpmndi:BPMNShape id=""Event_0dt2py5_di"" bpmnElement=""MessageEventId"">
-        <dc:Bounds x=""272"" y=""402"" width=""36"" height=""36"" />
+      <bpmndi:BPMNShape id=""Event_19p8um1_di"" bpmnElement=""Event_19p8um1"">
+        <dc:Bounds x=""270"" y=""122"" width=""36"" height=""36"" />
       </bpmndi:BPMNShape>
-      <bpmndi:BPMNShape id=""Activity_11o6o59_di"" bpmnElement=""sontask"">
-        <dc:Bounds x=""440"" y=""460"" width=""100"" height=""80"" />
+      <bpmndi:BPMNShape id=""Event_1cgqww1_di"" bpmnElement=""Event_1cgqww1"">
+        <dc:Bounds x=""502"" y=""122"" width=""36"" height=""36"" />
       </bpmndi:BPMNShape>
-      <bpmndi:BPMNShape id=""Activity_13ygffw_di"" bpmnElement=""map2items"">
-        <dc:Bounds x=""240"" y=""460"" width=""100"" height=""80"" />
+      <bpmndi:BPMNShape id=""Activity_13n23id_di"" bpmnElement=""Activity_13n23id"">
+        <dc:Bounds x=""350"" y=""100"" width=""100"" height=""80"" />
       </bpmndi:BPMNShape>
-      <bpmndi:BPMNShape id=""Event_0ss5h3c_di"" bpmnElement=""Event_0ss5h3c"">
-        <dc:Bounds x=""612"" y=""482"" width=""36"" height=""36"" />
-      </bpmndi:BPMNShape>
-      <bpmndi:BPMNShape id=""Event_085mbwq_di"" bpmnElement=""Event_085mbwq"">
-        <dc:Bounds x=""322"" y=""482"" width=""36"" height=""36"" />
-      </bpmndi:BPMNShape>
-      <bpmndi:BPMNEdge id=""Flow_17n3a2y_di"" bpmnElement=""Flow_17n3a2y"">
-        <di:waypoint x=""290"" y=""148"" />
-        <di:waypoint x=""290"" y=""190"" />
+      <bpmndi:BPMNEdge id=""Flow_11x64re_di"" bpmnElement=""Flow_11x64re"">
+        <di:waypoint x=""306"" y=""140"" />
+        <di:waypoint x=""350"" y=""140"" />
       </bpmndi:BPMNEdge>
-      <bpmndi:BPMNEdge id=""Flow_1f35s69_di"" bpmnElement=""Flow_1f35s69"">
-        <di:waypoint x=""290"" y=""270"" />
-        <di:waypoint x=""290"" y=""300"" />
+      <bpmndi:BPMNEdge id=""Flow_0h4nf3f_di"" bpmnElement=""Flow_0h4nf3f"">
+        <di:waypoint x=""450"" y=""140"" />
+        <di:waypoint x=""502"" y=""140"" />
       </bpmndi:BPMNEdge>
-      <bpmndi:BPMNEdge id=""Flow_05kznqg_di"" bpmnElement=""Flow_05kznqg"">
-        <di:waypoint x=""290"" y=""380"" />
-        <di:waypoint x=""290"" y=""402"" />
+      <bpmndi:BPMNEdge id=""Flow_0g8wv39_di"" bpmnElement=""Flow_0g8wv39"">
+        <di:waypoint x=""192"" y=""100"" />
+        <di:waypoint x=""230"" y=""100"" />
       </bpmndi:BPMNEdge>
-      <bpmndi:BPMNEdge id=""Flow_13vyc9a_di"" bpmnElement=""Flow_13vyc9a"">
-        <di:waypoint x=""290"" y=""438"" />
-        <di:waypoint x=""290"" y=""460"" />
-      </bpmndi:BPMNEdge>
-      <bpmndi:BPMNEdge id=""Flow_1qj8tpk_di"" bpmnElement=""Flow_1qj8tpk"">
-        <di:waypoint x=""290"" y=""540"" />
-        <di:waypoint x=""290"" y=""570"" />
-        <di:waypoint x=""480"" y=""570"" />
-        <di:waypoint x=""480"" y=""540"" />
-      </bpmndi:BPMNEdge>
-      <bpmndi:BPMNEdge id=""Flow_1n6d1n8_di"" bpmnElement=""Flow_1n6d1n8"">
-        <di:waypoint x=""540"" y=""500"" />
-        <di:waypoint x=""612"" y=""500"" />
+      <bpmndi:BPMNEdge id=""Flow_1vdwbwx_di"" bpmnElement=""Flow_1vdwbwx"">
+        <di:waypoint x=""580"" y=""90"" />
+        <di:waypoint x=""702"" y=""90"" />
       </bpmndi:BPMNEdge>
     </bpmndi:BPMNPlane>
   </bpmndi:BPMNDiagram>
 </bpmn:definitions>
-
 
 ";
 
@@ -191,6 +113,7 @@ namespace Playground
                 .Create<SequenceProcessorBuilder>()
                 .UsingElementHandler(StartEvent.ElementTypeName, new StartEventHandler())
                 .UsingElementHandler(ExclusiveGateway.ElementTypeName, new ExclusiveGatewayHandler())
+                .UsingElementHandler(SubProcess.ElementTypeName, new SubProcessHandler())
                 .UsingElementHandler(ScriptTask.ElementTypeName, new ScriptTaskHandler())
                 .UsingElementHandler(BpmnSequenceElements.Task.ElementTypeName, new TaskHandler())
                 .UsingElementHandler(IntermediateCatchEvent.ElementTypeName, new IntermediateCatchEventHandler())
---
+++ src/BpmnInterpreter/TraTech.BpmnInterpreter/Core/SequenceElements/SubProcess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TraTech.BpmnInterpreter.Core.SequenceElements
+{
+    public class SubProcess : BpmnSequenceElement
+    {
+
+        public static readonly string ElementTypeName = "subProcess";
+
+        public SubProcess(
+            XElement self,
+            IEnumerable<BpmnSequenceElement>? previousElements = null,
+            IEnumerable<BpmnSequenceElement>? nextElements = null
+        ) : base(self, previousElements, nextElements)
+        {
+
+        }
+    }
+}
